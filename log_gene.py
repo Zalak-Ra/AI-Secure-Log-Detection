d40b8999738762abdb02b9b16be3ec409dc4250d
Zalak-Ra <zalakranparia@gmil.com>
2026-03-20 23:27:10 +0530

    first commit

---
+++ log_gene.py
@@ -0,0 +1,89 @@
+import random
+import time
+import json
+from kafka import KafkaProducer
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+avg_mem_val = max(0, random.gauss(5.542812e+00, 1.062084e+01))
+max_mem_val = max(0, random.gauss(1.069742e+01, 5.770631e+01))
+
+memory_leak = False
+MEM_LIMIT = 200
+machine_id = ["server_alibaba_01","server_alibaba_02","server_alibaba_03","server_alibaba_04","server_alibaba_05"] 
+
+print(f"Starting advanced Alibaba cluster simulation for {machine_id}...")
+
+while True:
+    if random.random() < 0.99:
+        cpu_val = max(0, random.gauss(2.589425e+02, 5.674885e+02))
+    else:
+        cpu_val = random.randint(
+            int(2.589425e+02 + 3 * 5.674885e+02),
+            int(2.589425e+02 + 5 * 5.674885e+02)
+        )
+
+    # 2. GPU
+    if random.random() < 0.99:
+        gpu_val = max(0, random.gauss(1.596829e+01, 2.902764e+01))
+        avg_gpu_val = max(0, random.gauss(2.060904e+00, 6.057909e+00))
+        max_gpu_val = max(0, random.gauss(2.795080e+00, 7.646590e+00))
+    else:
+        gpu_val = 0
+        if random.random() < 0.99:
+            avg_gpu_val = max(0, random.gauss(2.060904e+00, 6.057909e+00))
+            max_gpu_val = max(0, random.gauss(2.795080e+00, 7.646590e+00))
+        else:
+            avg_gpu_val = 16
+            max_gpu_val = 16
+
+    # 3. IO (Network)
+    if random.random() < 0.99:
+        re_val = max(0, random.gauss(2.611418e+08, 6.745454e+08))
+        wr_val = max(0, random.gauss(6.187359e+07, 2.623019e+08))
+        re_co_val = max(0, random.gauss(1.124878e+04, 2.735763e+04))
+        wr_co_val = max(0, random.gauss(8.944866e+03, 8.220602e+04))
+    else:
+        re_val = max(0, random.gauss(2.611418e+08 * 5, 6.745454e+08 * 5))
+        wr_val = max(0, random.gauss(6.187359e+07 * 5, 2.623019e+08 * 5))
+        re_co_val = max(0, random.gauss(1.124878e+04 * 5, 2.735763e+04 * 5))
+        wr_co_val = max(0, random.gauss(8.944866e+03 * 5, 8.220602e+04 * 5))
+
+    # 4. Memory Leak Logic
+    if not memory_leak and random.random() < 0.01:
+        memory_leak = True
+        print("MEMORY LEAK STARTED")
+
+    if memory_leak:
+        avg_mem_val += random.uniform(0.5, 2)
+        max_mem_val += random.uniform(1, 4)
+        if max_mem_val > MEM_LIMIT:
+            memory_leak = False
+            avg_mem_val = max(0, random.gauss(5.542812e+00, 1.062084e+01))
+            max_mem_val = max(0, random.gauss(1.069742e+01, 5.770631e+01))
+    else:
+        avg_mem_val += random.uniform(-0.5, 0.5)
+        max_mem_val += random.uniform(-1, 1)
+
+    # 5. The Final Payload
+    logs = {
+        "timestamp": time.time(),    
+        "machine": random.choice(machine_id),        
+        "cpu_usage": round(cpu_val, 2),
+        "gpu_wrk_util": round(gpu_val, 2),
+        "avg_mem": round(avg_mem_val, 2),
+        "max_mem": round(max_mem_val, 2),
+        "avg_gpu_wrk_mem": round(avg_gpu_val, 2),
+        "max_gpu_wrk_mem": round(max_gpu_val, 2),
+        "read": int(re_val),
+        "write": int(wr_val),
+        "read_count": int(re_co_val),
+        "write_count": int(wr_co_val)
+    }
+    
+    producer.send("server_logs", logs)
+    print(f"Sent: CPU {logs['cpu_usage']} | Max Mem {logs['max_mem']}")
+    time.sleep(0.5)
